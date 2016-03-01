4621422b4f8590c2b470af5ff7389f9f91895884
Patryk Hołda <Patryk Hoda>
2016-03-01 21:47:49 +0100

    first commit1

---
+++ GeneratorNapisowCyklicznych.cpp
@@ -0,0 +1,16 @@
+// GeneratorNapisowCyklicznych.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+	cout << "hello";	
+    return 0;
+}
+
